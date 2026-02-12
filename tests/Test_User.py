ab60bf274403c024bb09300dcd1afca495f8deab
cainlucastapp <cainlucastapp@agentformula.com>
2026-02-12 11:53:57 -0800

    TDD planning

--- tests/Test_User.py
+++ tests/Test_User.py
@@ -1,5 +1,39 @@
 # tests/Test_User.py
 
 
+# Requirements
 import pytest
 from lib.models.User import User
+
+
+# Mocks / Helpers
+
+
+# Test User creation
+    # Pass
+    # Fail
+    # Edge cases
+
+
+# Test User editing
+    # Pass
+    # Fail
+    # Edge cases
+
+
+# Test User deletion
+    # Pass
+    # Fail
+    # Edge cases
+
+
+# Test User properties: user_id, user_name, user_email
+    # Pass
+    # Fail
+    # Edge cases
+
+
+# Test CLI output for User
+    # Pass
+    # Fail
+    # Edge cases
